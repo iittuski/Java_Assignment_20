15e26b10a7d830e1627fde5fa0b64e3ea32a0a1b
iittuski <iittuski@gmail.com>
2016-04-15 09:00:20 -0700

    j20
    j20

---
+++ j20/src/j20/axis.java
@@ -0,0 +1,15 @@
+package j20;
+
+public class axis extends superbank{
+	void rateofinterest() {
+		System.out.println("Rate is 7%");
+		super.rateofinterest();
+		
+	}
+
+	public static void main(String args[]) {
+		axis axis = new axis();
+		axis.rateofinterest();
+	}
+
+}
---
+++ j20/src/j20/superbank.java
@@ -0,0 +1,10 @@
+package j20;
+
+public class superbank{
+
+	void rateofinterest(){
+		System.out.println("Rate of interest is of common bank");
+	}
+	
+	
+}
